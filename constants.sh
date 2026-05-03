d069bd9f5cf3486c08534c5e2288488a46ae27a3
Gokberk Ozsoy <gokozsoy20@gmail.com>
2026-05-03 22:20:46 +0100

    add selectable game states, and mv constants

---
+++ constants.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# text colors
+RED="\e[31m" 
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+
+# background colors
+BG_RED="\e[41m"
+BG_YELLOW="\e[43m"
+
+# styling
+ITALICS="\e[3m"
+NO_ITALICS="\e[23m"
+
+# misc
+MOVE_CURSOR_UP="\033[1A"
+DELETE_LINE="\033[2K"
+RESET="\e[0m"
+SPANISH_FLAG="\t${BG_RED}          ${RESET}\n\t${BG_YELLOW}          ${RESET}\n\t${BG_YELLOW}          ${RESET}\n\t${BG_RED}          ${RESET}\n"
--- game.sh
+++ game.sh
@@ -1,68 +1,40 @@
 #!/bin/bash
 
+source constants.sh
 source dev.sh
 
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-RESET="\e[0m"
-ITALICS="\e[3m"
-NO_ITALICS="\e[23m"
-MOVE_CURSOR_UP="\033[1A"
-DELETE_LINE="\033[2K"
-
-printf "${BLUE}Welcome to Conjugation Game!${RESET}\n"
+printf "${BLUE}Welcome to Conjugation Game!${RESET}\n\n"
+printf "$SPANISH_FLAG\n"
 
 SUBJECTS=("yo" "tu" "el/ella/usted" "nosotros" "vosotros" "ellos/ellas/Ustedes")
 
-verb=tocar
+# TODO: temporary arrays
+VERBS=("tocar" "jugar" "ser" "nadar")
+TENSES=("Presente" "Preterito perfecto" "Preterito")
 
-# find verb conjugations and store in arr
-conjugations_str=$(fetch_conjugations "$verb" "Presente" "Indicativo")
-IFS=',' read -ra conjugations_arr <<< "$conjugations_str"
 
-# convert temp_subject indices list to arr
-subjects_arr=()
-while IFS= read -r line; do
-    subjects_arr+=($line)
-done < <(shuffle_indices 0 5)
+#for verb in "${VERBS[@]}"; do
+#    quiz_verb "${verb}" "${TENSES[0]}"
+#    printf "\n\n"
+#done
 
-# single verb loop
-conjugated_wrong=false
-curr=0
-
-printf "$verb ${ITALICS}means${NO_ITALICS} \"${conjugations_arr[${#conjugations_arr[@]}-1]}\"\n"
-
-while [[ $curr -lt "${#SUBJECTS[@]}" ]]; do
-    # fetch curr subject
-    subject_idx=${subjects_arr[$curr]}
-
-    # get subject word
-    temp_subject=${SUBJECTS[$subject_idx]}
-
-    # fetch correct conjugation
-    conj_gt=${conjugations_arr[$subject_idx]}
-
-    if [[ $conjugated_wrong == "true" ]]; then
-        printf "${RED}"$verb" ${ITALICS}$temp_subject${NO_ITALICS} ${RESET}"
-    else
-        printf ""$verb" ${ITALICS}$temp_subject${NO_ITALICS} "
-    fi
-    read input
-
-    printf "${MOVE_CURSOR_UP}${DELETE_LINE}"
-
-    if [[ $input == $conj_gt ]]; then    
-        printf "${GREEN}"$verb" ${ITALICS}$temp_subject${NO_ITALICS} $input${RESET}\n"
-        conjugated_wrong=false
-        ((curr++))
-    else
-        conjugated_wrong=true
-    fi
-
-    
+universe_options=("all verbs" "only my verbs")
+printf "${BLUE}Select universe:${RESET}\n"
+select selected_universe in "${universe_options[@]}"; do
+    break
 done
 
+tense_options=("${TENSES[@]}" "All")
+printf "${BLUE}Select tense:${RESET}\n"
+select selected_tense in "${tense_options[@]}"; do
+    break
+done
 
+subject_options=("Given verb, ask all 6 subjects" "Given verb, ask 1 random subject")
+printf "${BLUE}Select subject mode:${RESET}\n"
+select selected_subject in "${subject_options[@]}"; do
+    break
+done
+
+echo $selected_universe $selected_tense $selected_subject
 
